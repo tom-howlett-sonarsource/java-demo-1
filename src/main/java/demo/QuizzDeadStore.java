5b28e085aaa053e0ee3ed682bee78f21edebc69c
Johann Beleites <johann.beleites@sonarsource.com>
2022-10-04 17:01:09 +0200

    Formatting

--- src/main/java/demo/QuizzDeadStore.java
+++ src/main/java/demo/QuizzDeadStore.java
@@ -9,25 +9,25 @@ import org.slf4j.LoggerFactory;
 public class QuizzDeadStore {
 
   private static final Logger LOG = LoggerFactory
-      .getLogger(QuizzDeadStore.class);
+    .getLogger(QuizzDeadStore.class);
 
   public void playWith(int number, int playerAge) {
     int amountOfMoney;
     switch (number) {
-    case 0:
-      amountOfMoney = 1_000;
-      break;
-    case 42:
-      switch (playerAge) {
+      case 0:
+        amountOfMoney = 1_000;
+        break;
       case 42:
-        amountOfMoney = 10_000;
-        break;
+        switch (playerAge) {
+          case 42:
+            amountOfMoney = 10_000;
+            break;
+          default:
+            amountOfMoney = 5_000;
+            break;
+        }
       default:
-        amountOfMoney = 5_000;
-        break;
-      }
-    default:
-      throw new RuntimeException("Game Over!");
+        throw new RuntimeException("Game Over!");
     }
     give(amountOfMoney);
   }
